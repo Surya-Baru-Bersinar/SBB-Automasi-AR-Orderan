190757323dc0579e96e74012bb7cee5be9baf74a
Ihfandi <39288119+ihfandica@users.noreply.github.com>
2026-08-07 11:00:06 +0700

    Adj patch

--- Dapur/2_CleanerAccGiro.py
+++ Dapur/2_CleanerAccGiro.py
@@ -18,26 +18,45 @@ if not run_giro:
     print("--> Proses GIRO di-skip berdasarkan config.conf.")
     exit()
 
-df = pd.read_excel('Giro.xls', header=None)
+df = pd.read_excel('Giro.xls', header=4)
 
-data_bersih = []
-for i in range(len(df)):
-    baris = df.iloc[i].dropna().tolist()
-    if len(baris) >= 9:
-        data_bersih.append(baris)
+df.columns = df.columns.astype(str).str.strip()
 
-kolom = [
-    'No. Pelanggan', 'Nama Pelanggan', 'Tgl Faktur', 
-    'No. Faktur. (SO)', 'No. Form', 'Total Diterima', 
-    'Nilai terima', 'Nama Bank', 'Tgl Cek'
-]
+mapping_kolom = {
+    'Kode': 'No. Pelanggan',
+    'Nama Pelanggan': 'Nama Pelanggan',
+    'Tgl Faktur': 'Tgl Faktur',
+    'No': 'No. Faktur. (SO)',
+    'No. Faktur': 'No. Faktur. (SO)',
+    'No Cek': 'No. Form',
+    'Nilai Faktur': 'Total Diterima',
+    'Nilai Dibayar': 'Nilai terima',
+    'Bayar Via': 'Nama Bank',
+    'Tgl Bayar': 'Tgl Cek'
+}
 
-hasil_df = pd.DataFrame(data_bersih, columns=kolom)
+kolom_tersedia = [k for k in mapping_kolom.keys() if k in df.columns]
+hasil_df = df[kolom_tersedia].copy()
 
-hasil_df['Total Diterima'] = hasil_df['Total Diterima'].astype(str).str.replace(',', '.', regex=False)
-hasil_df['Total Diterima'] = pd.to_numeric(hasil_df['Total Diterima'], errors='coerce')
-hasil_df['Nilai terima'] = hasil_df['Nilai terima'].astype(str).str.replace(',', '.', regex=False)
-hasil_df['Nilai terima'] = pd.to_numeric(hasil_df['Nilai terima'], errors='coerce')
+hasil_df = hasil_df.rename(columns=mapping_kolom)
+
+if 'No. Pelanggan' in hasil_df.columns:
+    hasil_df = hasil_df.dropna(subset=['No. Pelanggan'])
+    
+    hasil_df['No. Pelanggan'] = hasil_df['No. Pelanggan'].astype(str).str.strip()
+    hasil_df = hasil_df[~hasil_df['No. Pelanggan'].isin(['', 'nan', 'None'])]
+
+# if 'No. Faktur. (SO)' in hasil_df.columns:
+#    hasil_df['No. Faktur. (SO)'] = hasil_df['No. Faktur. (SO)'].astype(str).str.replace('INV/', '', regex=False)
+
+if 'Total Diterima' in hasil_df.columns:
+    hasil_df['Total Diterima'] = hasil_df['Total Diterima'].astype(str).str.replace(',', '.', regex=False)
+    hasil_df['Total Diterima'] = pd.to_numeric(hasil_df['Total Diterima'], errors='coerce')
+
+if 'Nilai terima' in hasil_df.columns:
+    hasil_df['Nilai terima'] = hasil_df['Nilai terima'].astype(str).str.replace(',', '.', regex=False)
+    hasil_df['Nilai terima'] = pd.to_numeric(hasil_df['Nilai terima'], errors='coerce')
+
 hasil_df.to_excel('Giro_temp.xlsx', index=False)
 
-print("--> File Giro_temp.xlsx telah berhasil dibuat dengan format angka pada kolom Total Diterima.")
+print("--> File Giro_temp.xlsx telah berhasil dibuat")
